ce7bd9c55c2d7db5b44753f454697e02a6741332
D3B1RUM4N <elies1.mek03@gmail.com>
2023-12-28 21:33:28 +0100

    :art: new home

--- LS-Transit/resources/views/account.blade.php
+++ LS-Transit/resources/views/account.blade.php
@@ -3,9 +3,62 @@
 @section('title','Account')
 
 @section('content')
-	<p>
-		Bonjour {{ session('user')->login }} !<br>
-		Bienvenu sur ton compte.
-	</p>
-	<p><a href="signout">Sign out</a></p>
+@if($user = session('user')) @endif
+    <h1>Bienvenue {{ $user->login }}</h1>
+    <div class="container">
+        <div class="panel">
+            <div class="panel-item">
+                <h4>Distance parcourue :</h4>
+                <p>{{ $user->km }} km</p>
+            </div>
+            <div class="panel-item">
+                <h4>Total des factures :</h4>
+                <p>{{ $user->montant }} $</p>
+            </div>
+            <!-- Ajoutez d'autres éléments ici au besoin -->
+        </div>
+    </div>
+    <p><a href="signout">Se deconnecter</a></p>
 @endsection
+
+<style>
+
+
+.panel {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.panel-item {
+    flex: 0 0 calc(33.3333% - 20px); /* Trois éléments par ligne avec un espace de 20px entre eux */
+    margin-bottom: 20px;
+    background-color: #fff;
+    padding: 15px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    color: #333;
+}
+
+h4 {
+    color: #555;
+    margin-bottom: 10px;
+}
+
+p {
+    margin: 0;
+    color: #333;
+}
+
+a {
+    color: #3498db;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+</style>
